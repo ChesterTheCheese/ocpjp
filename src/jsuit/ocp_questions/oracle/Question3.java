b7ee93813b2c942fc4a03f968eb93705f5332f7b
Jakub Sengerski <senger@MacBook-Pro-Jakub.local>
2017-12-30 14:38:53 +0100

    Question3. Example upgrade

--- src/jsuit/ocp_questions/oracle/Question3.java
+++ src/jsuit/ocp_questions/oracle/Question3.java
@@ -5,39 +5,35 @@ import java.io.IOException;
 
 public class Question3 {
 
-    public static void main(String[] args){
-        System.out.println("Question3.main");
+    public static void main(String[] args) {
+        try (MyResource1 r1 = new MyResource1(); MyResource2 r2 = new MyResource2();) {
+            System.out.print("T ");
+        } catch (IOException ioe) {
+            System.out.print("IOE ");
+        } finally {
+            System.out.print("F ");
+        }
+        try (MyResource2 r2 = new MyResource2(); MyResource1 r1 = new MyResource1()) {
+            System.out.print("T ");
+        } catch (IOException ioe) {
+            System.out.print("IOE ");
+        } finally {
+            System.out.print("F ");
+        }
     }
 
     static class MyResource1 implements Closeable {
         public void close() throws IOException {
-            System.out.print("1");
+            System.out.print("1 ");
             throw new IOException();
         }
+
     }
+
     static class MyResource2 implements AutoCloseable {
         public void close() throws IOException {
             System.out.print("2 ");
-//            throw new IOException();
-        }
-    }
-
-    static class TestRes {
-        public static void main(String[] args) {
-            try (MyResource1 r1 = new MyResource1(); MyResource2 r2 = new MyResource2();) {
-                System.out.print("T ");
-            } catch (IOException ioe) {
-                System.out.print("IOE ");
-            } finally {
-                System.out.print("F ");
-            }
-            try (MyResource2 r2 = new MyResource2(); MyResource1 r1 = new MyResource1()) {
-                System.out.print("T ");
-            } catch (IOException ioe) {
-                System.out.print("IOE ");
-            } finally {
-                System.out.print("F ");
-            }
+            throw new IOException();
         }
     }
 }
