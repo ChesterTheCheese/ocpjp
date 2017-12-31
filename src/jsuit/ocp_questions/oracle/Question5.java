5f0020d2d9e6e8d1e64c011dd7e9563063f55f25
Jakub Sengerski <senger@MacBook-Pro-Jakub.local>
2017-12-31 14:42:54 +0100

    Incorrect commit message

---
+++ src/jsuit/ocp_questions/oracle/Question5.java
@@ -0,0 +1,23 @@
+package jsuit.ocp_questions.oracle;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+public class Question5 {
+
+//    class CallerThread1 implements Callable<String> {
+//        public String call() throws Exception { return "thread";}
+//    }
+//    class CallerThread2 implements Callable {
+//        public void call() {}
+//    }
+//    class CallerThread3 extends Callable {
+//        public String call() throws IOException {return "";}
+//    }
+//    class CallerThread4 implements Callable<String> {
+//        public String call(String s) { return "thread";}
+//    }
+//    class CallerThread5 extends Callable<String> {
+//        public void callable(String s) throws Exception {}
+//    }
+}
---
+++ src/jsuit/ocp_questions/oracle/package-info.java
@@ -0,0 +1,5 @@
+package jsuit.ocp_questions.oracle;
+
+/*
+ http://education.oracle.com/education/downloads/Exam809_SampleQuestion.pdf
+ */
